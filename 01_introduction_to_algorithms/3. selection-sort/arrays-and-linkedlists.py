71d1721981185c33c1689178a24396e35a62b576
Joao Lima <joaoplima@ua.pt>
2020-09-30 15:37:52 +0200

    linkedlist

---
+++ 01_introduction_to_algorithms/3. selection-sort/arrays-and-linkedlists.py
@@ -0,0 +1,9 @@
+# Arrays Vs List
+
+# Insert: Lists are better if you want to insert elements into the middle - O(1)
+# Delete: List are better - O(1)
+# Read: Arrays are better - O(1)
+
+# Two way of access: 
+# Random access - Can jump directly to the element (Arrays)
+# Sequential access -  reading the elements one by one, starting at the first elemenet (Linkedlist)
